947421b1d5e75374691cf5a3ae2dd2c83903db88
lapcharty <ascii1011@gmail.com>
2022-10-30 02:40:21 -0400

    fixed executable issue with vscode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+//use petgraph::graph::{UnGraph};
+//use petgraph::algo::{dijkstra, min_spanning_tree};
+//use petgraph::data::FromElements;
+//use petgraph::dot::{Dot, Config};
+
+
+
 fn main() {
     println!("Hello, world!");
+
+    // Create an undirected graph with `i32` nodes and edges with `()` associated data.
+    /*
+    let g = UnGraph::<i32, ()>::from_edges(&[
+        (1, 2), (2, 3), (3, 4),
+        (1, 4)]);
+        */
+    // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
+    //let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+
+    //println!("node_map: {}", node_map);
 }
